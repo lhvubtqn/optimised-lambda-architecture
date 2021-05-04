a6f326689e35873fe14210e7f5a4d2e8613ebef0
Hoang-Vu Le <vulh4@vng.com.vn>
2021-05-04 10:49:45 +0700

    remove hard code app name & spark master

--- lambda/apps/run_kafka_producer.sh
+++ lambda/apps/run_kafka_producer.sh
@@ -2,7 +2,10 @@
 
 cd "$(dirname "$0")"
 
+docker stop iot-kafka-producer 2> /dev/null || true
+docker rm iot-kafka-producer 2> /dev/null || true
+
 docker run -d --name iot-kafka-producer \
     --network lambda-network \
-    --volumes iot-kafka-producer/target/iot-kafka-producer-1.0.0.jar /iot-kafka-producer-1.0.0.jar \
+    --volume $(pwd)/iot-kafka-producer/target/iot-kafka-producer-1.0.0.jar:/iot-kafka-producer-1.0.0.jar \
     java:8 java -jar /iot-kafka-producer-1.0.0.jar
--- lambda/apps/submit_spark_batch_job.sh
+++ lambda/apps/submit_spark_batch_job.sh
@@ -3,7 +3,9 @@
 cd "$(dirname "$0")"
 
 docker exec spark-master spark-submit \
-    --deploy-mode cluster \
+    --deploy-mode client \
+    --master spark://spark-master:7077 \
+    --properties-file /opt/bitnami/spark/conf/spark-defaults.conf \
     --class com.apssouza.iot.processor.BatchProcessor \
-    --master spark://localhost:7077 \
+    --name "IoT Data Batch Processor" \
     /opt/spark/apps/iot-spark-processor-1.0.0.jar
