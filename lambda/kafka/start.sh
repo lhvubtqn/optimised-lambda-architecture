a3b23f695b38541e6045f93ceaa35c8b34af705a
Hoang-Vu Le <vulh4@vng.com.vn>
2021-05-04 10:18:44 +0700

    remove datagen

--- lambda/kafka/start.sh
+++ lambda/kafka/start.sh
@@ -16,16 +16,6 @@ if [[ "$(docker network ls | grep lambda-network 2> /dev/null)" == "" ]]; then
   docker network create -d bridge lambda-network
 fi
 
-mkdir datagen/plugins 2> /dev/null || true
-
-if [ ! -d ./datagen/plugins/confluentinc-kafka-connect-datagen-0.4.0 ]; then
-  echo -e "\n⏳ Downloading connect-datagen:0.4.0"
-  (cd ./datagen/plugins && \
-  curl -O https://d1i4a15mxbxib1.cloudfront.net/api/plugins/confluentinc/kafka-connect-datagen/versions/0.4.0/confluentinc-kafka-connect-datagen-0.4.0.zip && \
-  unzip -q confluentinc-kafka-connect-datagen-0.4.0.zip && \
-  rm confluentinc-kafka-connect-datagen-0.4.0.zip)
-fi
-
 echo -e "\n🐳 Starting Kafka cluster"
 docker-compose up -d --remove-orphans
 
@@ -43,22 +33,5 @@ done
 
 echo -e "\n⏳ Creating Kafka topics"
 docker exec -it $KAFKA_CONTAINER_NAME kafka-topics --zookeeper zookeeper:2181 --create --topic iot-data-event --partitions 8 --replication-factor 1 --config retention.bytes=102400
-
-CONTAINER_NAME=datagen
-echo -e "\n⏳ Waiting for Datagen Container to be up and running"
-while true
-do
-  if [ $(docker logs $CONTAINER_NAME 2>&1 | grep "REST resources initialized; server is started and ready to handle requests" >/dev/null; echo $?) -eq 0 ]; then
-    echo
-    break
-  fi
-  printf "."
-  sleep 1
-done
-
-echo -e "\n⏳ Creating Datagen Connector..."
-docker exec -it $CONTAINER_NAME curl -s -X DELETE http://localhost:8083/connectors/$1 > /dev/null
-docker exec -it $CONTAINER_NAME curl -s -X POST -H "Content-Type: application/json" -d "@/usr/share/datagen/configs/datagen_connector.config" http://localhost:8083/connectors > /dev/null
-echo -e "Created Datagen Connector." 
 
 exit 0
