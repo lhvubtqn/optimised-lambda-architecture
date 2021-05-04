5192a17a659d5973325387d0930dc919802e922a
Hoang-Vu Le <vulh4@vng.com.vn>
2021-05-04 10:17:47 +0700

    use docker to produce iot data

--- lambda/apps/run_kafka_producer.sh
+++ lambda/apps/run_kafka_producer.sh
@@ -2,4 +2,7 @@
 
 cd "$(dirname "$0")"
 
-java -jar iot-kafka-producer/target/iot-kafka-producer-1.0.0.jar
+docker run -d --name iot-kafka-producer \
+    --network lambda-network \
+    --volumes iot-kafka-producer/target/iot-kafka-producer-1.0.0.jar /iot-kafka-producer-1.0.0.jar \
+    java:8 java -jar /iot-kafka-producer-1.0.0.jar
