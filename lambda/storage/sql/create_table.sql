9bfc0d9545890db458370b9be9292204954cd180
Hoang-Vu Le <vulh4@vng.com.vn>
2021-06-27 00:25:26 +0700

    update sql script

---
+++ lambda/storage/sql/create_table.sql
@@ -0,0 +1,18 @@
+\c lametro
+
+DROP TABLE IF EXISTS bus_arrival;
+
+CREATE TABLE IF NOT EXISTS bus_arrival (
+   timestamp TIMESTAMP WITHOUT TIME ZONE NOT NULL,
+   bus_id INT NOT NULL,
+   route_id INT NOT NULL,
+   latitude DOUBLE PRECISION NOT NULL,
+   longitude DOUBLE PRECISION NOT NULL,
+   stop_id INT NOT NULL,
+   stop_lat DOUBLE PRECISION NOT NULL,
+   stop_lon DOUBLE PRECISION NOT NULL,
+   seconds_till_meet BIGINT NOT NULL
+);
+
+-- Step 2: Turn into hypertable
+SELECT create_hypertable('bus_arrival','timestamp');
