63b8b5513653e1bc61d5534fa05f33de7fae9c37
Hoang-Vu Le <vulh4@vng.com.vn>
2021-07-01 01:49:34 +0700

    create table bus_velocity

--- lambda/storage/sql/create_table.sql
+++ lambda/storage/sql/create_table.sql
@@ -4,13 +4,29 @@ DROP TABLE IF EXISTS bus_arrival;
 
 CREATE TABLE IF NOT EXISTS bus_arrival (
    timestamp TIMESTAMP WITHOUT TIME ZONE NOT NULL,
-   bus_id INT NOT NULL,
    route_id INT NOT NULL,
+   bus_id INT NOT NULL,
    latitude DOUBLE PRECISION NOT NULL,
    longitude DOUBLE PRECISION NOT NULL,
+   direction INT NOT NULL,
    stop_id INT NOT NULL,
    seconds_till_meet BIGINT NOT NULL
 );
 
--- Step 2: Turn into hypertable
 SELECT create_hypertable('bus_arrival','timestamp');
+SELECT add_retention_policy('bus_arrival', INTERVAL '24 hours');
+
+DROP TABLE IF EXISTS bus_velocity;
+
+CREATE TABLE IF NOT EXISTS bus_velocity (
+   timestamp TIMESTAMP WITHOUT TIME ZONE NOT NULL,
+   route_id INT NOT NULL,
+   bus_id INT NOT NULL,
+   latitude DOUBLE PRECISION NOT NULL,
+   longitude DOUBLE PRECISION NOT NULL,
+   direction INT NOT NULL,
+   velocity DOUBLE PRECISION NOT NULL
+);
+
+SELECT create_hypertable('bus_velocity','timestamp');
+SELECT add_retention_policy('bus_velocity', INTERVAL '24 hours');
