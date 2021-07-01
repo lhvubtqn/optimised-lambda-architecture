71aa0412e88137f37d07fe98b920bced53cc7776
Hoang-Vu Le <vulh4@vng.com.vn>
2021-07-02 02:16:09 +0700

    use nullable field

--- lambda/storage/sql/create_table.sql
+++ lambda/storage/sql/create_table.sql
@@ -3,17 +3,17 @@
 DROP TABLE IF EXISTS bus_arrival;
 
 CREATE TABLE IF NOT EXISTS bus_arrival (
-   timestamp TIMESTAMP WITHOUT TIME ZONE NOT NULL,
-   req_time TIMESTAMP WITHOUT TIME ZONE NOT NULL,
-   inserted_time TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT current_timestamp,
-   route_id INT NOT NULL,
-   bus_id INT NOT NULL,
-   latitude DOUBLE PRECISION NOT NULL,
-   longitude DOUBLE PRECISION NOT NULL,
-   velocity DOUBLE PRECISION NOT NULL,
-   direction INT NOT NULL,
-   stop_id INT NOT NULL,
-   seconds_till_meet BIGINT NOT NULL
+   timestamp TIMESTAMP WITHOUT TIME ZONE,
+   req_time TIMESTAMP WITHOUT TIME ZONE,
+   inserted_time TIMESTAMP WITHOUT TIME ZONE DEFAULT current_timestamp,
+   route_id INT,
+   bus_id INT,
+   latitude DOUBLE PRECISION,
+   longitude DOUBLE PRECISION,
+   velocity DOUBLE PRECISION,
+   direction INT,
+   stop_id INT,
+   seconds_till_meet BIGINT
 );
 
 SELECT create_hypertable('bus_arrival','timestamp');
