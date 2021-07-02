7d865d6b0bae76e824bda0355e7d0e48a96bb7cc
Hoang-Vu Le <vulh4@vng.com.vn>
2021-07-03 00:05:33 +0700

    Add hdfs data

--- lambda/storage/copy_data_to_hdfs.sh
+++ lambda/storage/copy_data_to_hdfs.sh
@@ -12,8 +12,8 @@ echo "--------------------------------------------------------------------------
 HDFS_CONTAINER=namenode
 
 echo -e "\n⏳ Copying data folders to HDFS..."
+unzip data.zip
 docker exec $HDFS_CONTAINER bash -c "hdfs dfs -copyFromLocal /home/data/static_data/* /ola/static_data"
-# docker exec $HDFS_CONTAINER bash -c "hdfs dfs -put /home/data/historical_data/DATA/* /ola/historical_data/"
-# docker exec $HDFS_CONTAINER bash -c "hdfs dfs -copyFromLocal /home/data/historical_data_2h/DATA/* /ola/historical_data"
+docker exec $HDFS_CONTAINER bash -c "hdfs dfs -copyFromLocal /home/data/aggregated_data/* /ola/aggregated_data"
 
 exit 0
