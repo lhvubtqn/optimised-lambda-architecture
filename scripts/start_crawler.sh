4fd0b41d0bd98d53e359e2b63900bccac84eeb5c
Hoang-Vu Le <vulh4@vng.com.vn>
2021-07-13 08:14:55 +0700

    add kafka & crawler

---
+++ scripts/start_crawler.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+cd "$(dirname "$0")"
+
+. constants.cfg
+
+# Download NodeJS
+if [ ! -d ./libs/node-v14.17.3-linux-x64 ]; then
+  echo -e "\n⏳ Downloading node-v14.17.3-linux-x64"
+  (cd ./libs && \
+  curl -O https://nodejs.org/dist/v14.17.3/node-v14.17.3-linux-x64.tar.xz && \
+  tar -xf node-v14.17.3-linux-x64.tar.xz && \
+  rm node-v14.17.3-linux-x64.tar.xz)
+fi
+
+# Export environment
+export NODE_HOME=$(pwd)/libs/node-v14.17.3-linux-x64
+export PATH=$NODE_HOME/bin:$PATH
+
+cd crawler
+mkdir logs
+
+$NODE_HOME/bin/npm install
+
+export WAIT_TIME_MS_BEFORE_START=5000
+export BOOTSTRAP_SERVERS=localhost:9092 
+export TOPIC=buses-location 
+export CRAWL_INTERVAL_MS=15000 
+
+nohup $NODE_HOME/bin/node index.js > logs/crawler.logs &
+
+pid=$(ps aux | grep '[n]ode index.js' | awk '{print $2}')
+
+echo -e "\n🏭 Crawler started with pid: $pid."
+
+exit 0
---
+++ scripts/start_kafka.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+cd "$(dirname "$0")"
+
+. constants.cfg
+
+# Download Apache Kafka
+if [ ! -d ./libs/kafka_2.12-2.6.0 ]; then
+  echo -e "\n⏳ Downloading kafka_2.12-2.6.0"
+  (cd ./libs && \
+  curl -O https://archive.apache.org/dist/kafka/2.6.0/kafka_2.12-2.6.0.tgz && \
+  tar -xf kafka_2.12-2.6.0.tgz && \
+  rm kafka_2.12-2.6.0.tgz)
+fi
+
+# Export environment
+export JAVA_HOME=$(pwd)/libs/jdk-11.0.11
+export KAFKA_HOME=$(pwd)/libs/kafka_2.12-2.6.0
+export PATH=$KAFKA_HOME/bin:$PATH
+
+cd $KAFKA_HOME
+
+mkdir logs
+
+echo -e "\n🏭 Starting Zookeeper..."
+nohup bin/zookeeper-server-start.sh config/zookeeper.properties > logs/zookeeper.logs &
+
+echo -e "\n🏭 Starting Kafka..."
+nohup bin/kafka-server-start.sh config/server.properties > logs/kafka.logs &
+
+echo -e "\n⏳ Waiting for Kafka Broker to be up and running"
+while true
+do
+  if [ $(cat logs/kafka.logs 2>&1 | grep "started (kafka.server.KafkaServer)" >/dev/null; echo $?) -eq 0 ]; then
+    echo
+    break
+  fi
+  printf "."
+  sleep 1
+done
+
+echo -e "\n⏳ Creating Kafka topics"
+bin/kafka-topics.sh --zookeeper localhost:2181 --create --topic buses-location --partitions 8 --replication-factor 1 --config retention.bytes=102400
+
+exit 0
